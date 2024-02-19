e88dfaf794a729dd55375b1cb15d7423a910eef5
Pranav <pranav.techiegeek@gmail.com>
2024-02-19 14:25:20 +0530

    add database file

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- SQLite Playground
+
+-- This playground contains Chinook database for writing queries. If you want to use a custom Sqlite Database, you can upload a `.db` or `.sqlite` file to use it for querying. Make sure to only have one database file in the file system.
+
+-- You can use the browser for executing queries or use the run button to see the query output in the terminal
+
+SELECT * FROM 'Album' 
+LIMIT 10
